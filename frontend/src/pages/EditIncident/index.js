82ca31db2263c1503e71280198afd32461553fa1
marcosjcs <jcs.marcos13@gmail.com>
2020-03-31 12:24:25 -0300

    Incident editting

---
+++ frontend/src/pages/EditIncident/index.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+
+export default function EditIncident(){
+    
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const history = useHistory();
+
+    const ongId = localStorage.getItem('ongId');
+    const incident = history.location.state;
+    useEffect(() => {
+        setTitle(incident.title);
+        setDescription(incident.description);
+        setValue(incident.value);
+    }, [incident.description, incident.id, incident.title, incident.value]);
+   
+
+    async function handleEditIncident(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+        try {
+            await api.put(`incidents/${incident.id}`, data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+            
+            history.push('/profile');
+        } catch (err){
+            alert('Erro ao atualizar caso, tente novamente.');
+        }
+    }
+
+    return (
+        <div className="edit-incident-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be The Hero"/>
+
+                    <h1>Editar caso</h1>
+                    <p>Altere as informações do caso para encontrar um herói que resolva isso.</p>
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#e02041"/>
+                        Voltar para o perfil
+                    </Link>
+                </section>
+
+                <form onSubmit={handleEditIncident}>
+                    <input 
+                        placeholder="Título do caso" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                        />
+                    <textarea 
+                        placeholder="Descrição" 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                        />
+                    <input 
+                        placeholder="Valor em reais" 
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        />
+
+                    <button className="button" type="submit">Atualizar</button>
+                </form>
+            </div>
+        </div>
+    )
+}
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import {FiPower, FiTrash2} from 'react-icons/fi';
+import {FiPower, FiTrash2, FiEdit} from 'react-icons/fi';
 
 import api from '../../services/api'
 
@@ -38,6 +38,11 @@ export default function Profile() {
         }
     }
 
+    function handleEditIncident(incident){
+        console.log(incident);
+        history.push("/incidents", { ...incident });
+    }
+
     function handleLogout(){
         localStorage.clear();
         history.push('/');
@@ -68,6 +73,9 @@ export default function Profile() {
                         <strong>VALOR:</strong>
                         <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(incident.value)}</p>
 
+                        <button onClick={() => handleEditIncident(incident)} type="button">
+                            <FiEdit size="20" color="a8a8b3"/>
+                        </button>
                         <button onClick={() => handleDeleteIncident(incident.id)} type="button">
                             <FiTrash2 size="20" color="a8a8b3"/>
                         </button>
